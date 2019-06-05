92b1ca954b10787ea183bd290e5263981568e6e3
Russell Hu <russelhu@umich.edu>
2019-06-05 17:23:57 -0400

    add initFunctionInstance in the start method; add the functions that use for calling the assemble method

--- src/ee/execution/JNITopend.h
+++ src/ee/execution/JNITopend.h
@@ -76,7 +76,7 @@ public:
 
     int32_t callJavaUserDefinedFunction();
     int32_t callJavaUserDefinedAggregateStart();
-    // int32_t callJavaUserDefinedAggregateAssemble();
+    int32_t callJavaUserDefinedAggregateAssemble();
     // int32_t callJavaUserDefinedAggregateWorkerEnd();
     // int32_t callJavaUserDefinedAggregateCoordinatorEnd();
     void resizeUDFBuffer(int32_t size);
@@ -103,7 +103,7 @@ private:
     jmethodID m_decodeBase64AndDecompressToBytesMID;
     jmethodID m_callJavaUserDefinedFunctionMID;
     jmethodID m_callJavaUserDefinedAggregateStartMID;
-    // jmethodID m_callJavaUserDefinedAggregateAssembleMID;
+    jmethodID m_callJavaUserDefinedAggregateAssembleMID;
     // jmethodID m_callJavaUserDefinedAggregateWorkerEndMID;
     // jmethodID m_callJavaUserDefinedAggregateCoordinatorEndMID;
     jmethodID m_resizeUDFBufferMID;
